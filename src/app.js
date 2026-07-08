e9aeab6619819a20bf9d6ea1ae7d6542d3357f8d
bielferr <gabrielferreiradesouza5513@gmail.com>
2026-07-08 18:59:12 -0300

    config app and server

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const  request  = require("express");
+
+const app = express()
+
+app.use(express.json() )
+
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,5 @@
+const app = require("./app")
+
+app.listen(5555, ()=>{
+    console.log("✅ server rurning on port 5555")
+})
