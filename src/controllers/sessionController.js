61be75bbd7f07dd67ac70006bdc05597a0b48d4c
bielferr <gabrielferreiradesouza5513@gmail.com>
2026-07-28 11:23:23 -0300

    feat: implement user auth JWT

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -1,11 +1,10 @@
 const prisma = require("../database/prisma")
 const bcrypt = require("bcrypt")
 const jsonwebtoken = require("jsonwebtoken")
-
+const authConfig = require("../config/auth")
 class SessionController {
     async create(req,res){
         const {email,password} = req.body
-     
         const user  = await prisma.user.findUnique({
             where:{
                 email,
@@ -18,22 +17,31 @@ class SessionController {
         })
         }
         
-      
         const passwordMatched = await bcrypt.compare(
             password,
             user.password
         )
+
 
         if(!passwordMatched){
             return res.status(401).json({ 
                 message:"Invalid email or password."})
         }
         
+        const secret = authConfig.jwt.secret
+        const expiresIn = authConfig.jwt.expiresIn
 
+        const token = jsonwebtoken.sign(
+        {id: user.id} ,
+            secret,{
+                expiresIn
+            }
+
+        )
         return res.json({
-            message:"Login successful!"
+            message:"Login successful!",
+            "token":token
         })
-       
     }
     
 }
